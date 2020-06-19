b217e9449769edf543b06e81c882b0d9b3b8f9a0
Ubirailson Jersy Soares de Medeiros <ubirailson.medeiros@dataprev.gov.br>
2020-06-19 18:25:33 -0300

    aulas ate a aula 4

--- springboot/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ springboot/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,26 +1,64 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
-import java.util.Arrays;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
 
-@Controller
+
+/*
+ * @Controller //espera-se que o redirecionamento seja para uma página
+ * 
+ */
+@RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
-	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> listar() {
-		Topico topico = new Topico("Duvida", "conteudo duvida", 
-				new Curso("CUrso teste", "categoria teste"));
-		Topico topico2 = new Topico("Duvida2", "conteudo duvida2", 
-				new Curso("CUrso teste2", "categoria teste2"));
-				
-		return Arrays.asList(topico, topico2);
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	@Autowired
+	private CursoRepository cursoRepository;
+	@GetMapping
+//	@RequestMapping("/topicos")
+/*
+ * @ResponseBody //usado pra dizer que o objeto de retorno deve ser serializado
+ * 				 // e retornado como resposta
+ * 			  	 // torna-se desnecessario quando se usa @RestController
+ * 				 // ao invés de @Controller
+ */		
+	public List<TopicoDto> listar(String nomeCurso) {
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
 	}
+	
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form,
+			UriComponentsBuilder uriBuilder) {
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+		URI uri = uriBuilder.path("topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+	
+	
 }
